db9edda9467e8ffbcd0f4330646b5d66bd6e1ea6
Kelsey Busser <busserk@gmail.com>
2020-11-15 19:03:02 -0500

    code

--- Develop/app.js
+++ Develop/app.js
@@ -18,6 +18,199 @@ const render = require("./lib/htmlRenderer");
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
+
+const engagementTeam = [];
+
+const confirmName = async (name) => {
+    if (name === '') {
+       return 'Incorrect answer';
+    };
+    return true;
+ };
+
+ const confirmNumber = async (name) => {
+    if (name === '') {
+       return 'Incorrect answer';
+    };
+    return true;
+ };
+ 
+ function validateEmail(name) 
+{if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(name))
+  {
+    return (true)
+  }
+    return("You have entered an invalid email address!")
+}
+
+
+//Write out validations; check out link about NaN; find out substitutes to check number validation; chain validations on first prompt
+
+function teamMember() {
+    // Ask questions to gather information about manager. Save to an manager object.
+    
+
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your manager's name?",
+                name: "name",
+                validate: confirmName
+            },
+            {
+                type: "input",
+                message: "What is your manager's id?",
+                name: "id",
+                validate: confirmNumber 
+            },
+            {
+                type: "input",
+                message: "What is your manager's email?",
+                name: "email",
+                validate: validateEmail
+            },
+            {
+                type: "input",
+                message: "What is your manager's office number?",
+                name: "officeNumber",
+                validate: confirmNumber
+            }
+        ])
+
+            .then(function (answers) {
+                let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+                engagementTeam.push(manager)
+                chooseMemberNext()
+            })
+            .catch(function(err) {
+                console.log(err);
+              });
+
+        // Determine if an engineer or intern will be added next.
+
+        async function chooseMemberNext() {
+            try {
+
+                let teamChoice = await inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'team',
+                        message: 'Which type of team member would you like to add',
+                        choices: ['Engineer', 'Intern', 'I don/t want to add anymore team members.']
+                    }
+                ]);
+
+                // Depending on the response, loop through questions to gather information and save to appropriate object
+                if (teamChoice.team === 'Engineer') {
+
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            message: "What is your engineer's name?",
+                            name: "name",
+                            validate: confirmName
+                        },
+                        {
+                            type: "input",
+                            message: "What is your engineer's id?",
+                            name: "id",
+                            validate: confirmNumber
+                        },
+                        {
+                            type: "input",
+                            message: "What is your engineer's email?",
+                            name: "email",
+                            validate: validateEmail
+                        },
+                        {
+                            type: "input",
+                            message: "What is your engineer's GitHub username?",
+                            name: "github",
+                            validate: confirmName
+                        }
+                    ])
+
+                        .then(function (answers) {
+                            let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+                            engagementTeam.push(engineer);
+                            chooseMemberNext();
+                        })
+                        .catch(function(err) {
+                            console.log(err);
+                          });
+
+                } else if (teamChoice.team === 'Intern') {
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            message: "What is your intern's name?",
+                            name: "name",
+                            validate: confirmName
+                        },
+                        {
+                            type: "input",
+                            message: "What is your intern's id?",
+                            name: "id",
+                            validate: confirmNumber
+                        },
+                        {
+                            type: "input",
+                            message: "What is your intern's email?",
+                            name: "email",
+                            validate: validateEmail 
+                        },
+                        {
+                            type: "input",
+                            message: "What is your intern's school?",
+                            name: "school",
+                            validate: confirmName
+                        }
+                    ])
+                        .then(function (answers) {
+                            let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+                            engagementTeam.push(intern);
+                            chooseMemberNext();
+                        })
+                        .catch(function(err) {
+                            console.log(err);
+                          });
+
+                } else {generateFile()}
+
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        // Loop back to original question for engineer or intern and begin again until user calls "I don't want to add
+        //anymore team members", at which point the loop stops
+
+  
+
+}
+
+teamMember();
+
+//Call the function
+
+
+
+
+function generateFile() {
+    fs.writeFileSync(outputPath, render(engagementTeam),"utf-8")
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
